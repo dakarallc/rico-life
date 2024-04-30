b694063aa0ab980bd1e4cdddb42ff537ebb37f19
kentoOnodera <kento.onodera@dakara.work>
2024-04-30 15:51:53 +0900

    fix: style修正

--- functions.php
+++ functions.php
@@ -58,7 +58,7 @@ function my_script_init() {
   $theme_url = get_template_directory_uri();
 
   wp_enqueue_style('animate', $theme_url . '/assets/css/vendor/animate.css', array(), '1.0.21', 'all');
-  wp_enqueue_style('styles', $theme_url . '/assets/sass/styles.css', array(), '1.0.28', 'all');
+  wp_enqueue_style('styles', $theme_url . '/assets/css/styles.css', array(), '1.0.28', 'all');
   wp_enqueue_script('ofi', $theme_url . '/assets/js/vendor/ofi.min.js', array('jquery'), '1.0.0', true);
   wp_enqueue_script('wow', $theme_url . '/assets/js/vendor/wow.min.js', array('jquery'), '1.0.0', true);
   wp_enqueue_script('cmn', $theme_url . '/assets/js/common.js', array('jquery'), '1.0.4', true);
