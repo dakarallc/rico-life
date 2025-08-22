305ff8e6e935d1f667fcb6d4e9cae93482bcc5ff
kentoOnodera <kento.onodera@dakara.work>
2025-08-22 16:25:13 +0900

    常時開催を追加

--- template-parts/eventArea.php
+++ template-parts/eventArea.php
@@ -17,47 +17,54 @@
   $event_query = new WP_Query($args);
 
   if ($event_query->have_posts()): ?>
-		<div class="event__slider swiper eventSwiper">
-			<div class="swiper-wrapper">
-				<?php while ($event_query->have_posts()):
+			<div class="event__slider swiper eventSwiper">
+				<div class="swiper-wrapper">
+					<?php while ($event_query->have_posts()):
 
-    	$event_query->the_post();
-    	$event_pic = get_field("event-pic");
-    	$is_always = get_field("is_always");
-    	$start_date = get_field("event_start_date");
-    	$end_date = get_field("event_end_date");
-    	$start_time = get_field("event_start_time");
-    	$end_time = get_field("event_end_time");
-    	$address = get_field("address");
-    	?>
-				<div class="event__slide swiper-slide">
-					<a href="<?php echo esc_url(get_permalink()); ?>" class="event__container">
-							<div class="event__image">
-							<img src="<?php echo esc_url($event_pic); ?>" alt="<?php the_title(); ?>">
+     	$event_query->the_post();
+     	$is_always = get_field("is_always");
+     	$event_pic = get_field("event-pic");
+     	$start_date = get_field("event_start_date");
+     	$end_date = get_field("event_end_date");
+     	$start_time = get_field("event_start_time");
+     	$end_time = get_field("event_end_time");
+     	$address = get_field("address");
+     	?>
+						<div class="event__slide swiper-slide">
+							<a href="<?php echo esc_url(get_permalink()); ?>" class="event__container">
+								<div class="event__image">
+									<img src="<?php echo esc_url($event_pic); ?>" alt="<?php the_title(); ?>">
+								</div>
+								<div class="event__info">
+									<h3 class="event__main-title"><?php the_title(); ?></h3>
+									<hr class="event__divider">
+									<p>完成見学会 ※完全予約制</p>
+									<div class="event__row">
+										<span class="event__icon"><i class="fa-regular fa-calendar"></i></span>
+										<span class="event__date">
+											<?php if ($is_always): ?>
+												常時開催
+											<?php else: ?>
+												<?php echo esc_html($start_date); ?> - <?php echo esc_html($end_date); ?>
+											<?php endif; ?>
+										</span>
+									</div>
+									<div class="event__row">
+										<span class="event__icon"><i class="fa-solid fa-location-dot"></i></span>
+										<span class="event__place"><?php echo esc_html($address); ?></span>
+									</div>
+								</div>
+							</a>
 						</div>
-						<div class="event__info">
-							<h3 class="event__main-title"><?php the_title(); ?></h3>
-							<hr class="event__divider">
-							<p>完成見学会 ※完全予約制</p>
-							<div class="event__row">
-								<span class="event__icon"><i class="fa-regular fa-calendar"></i></span>
-								<span class="event__date"><?php echo esc_html($start_date); ?> - <?php echo esc_html($end_date); ?></span>
-							</div>
-							<div class="event__row">
-								<span class="event__icon"><i class="fa-solid fa-location-dot"></i></span>
-								<span class="event__place"><?php echo esc_html($address); ?></span>
-							</div>
-						</div>
-					</a>
+					<?php
+     endwhile; ?>
 				</div>
-				<?php
-    endwhile; ?>
+				<div class="swiper-button-prev event-swiper-prev"></div>
+				<div class="swiper-button-next event-swiper-next"></div>
 			</div>
-			<div class="swiper-button-prev event-swiper-prev"></div>
-			<div class="swiper-button-next event-swiper-next"></div>
-		</div>
-		<?php wp_reset_postdata();else: ?>
-		<p>現在、開催中のイベントはありません。</p>
+			<?php wp_reset_postdata(); ?>
+		<?php else: ?>
+			<p>現在、開催中のイベントはありません。</p>
 		<?php endif;
   ?>
 
